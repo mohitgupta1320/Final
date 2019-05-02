bc6e4d7939638ecffae45d052f27edf0f861b56a
mohitgupta1320 <mohitgupta1320@gmail.com>
2019-05-01 22:56:35 -0700

    Commit_v1.0

---
+++ Script/Unit1.js
@@ -0,0 +1,19 @@
+﻿function Test1()
+{
+  Browsers.Item(btChrome).Run("chrome://extensions/");
+  let browser = Aliases.browser;
+  let page = browser.pageExtensions;
+  page.Keys("[F5]");
+  page.Wait();
+  page = browser.pageCreateANewRepository;
+  page.Wait();
+  let form = page.formNewRepository;
+  let textbox = form.textboxRepositoryName;
+  textbox.Click(86, 22);
+  textbox.SetText("Final");
+  form.buttonCreateRepository.ClickButton();
+  page = browser.pageFinal;
+  page.Wait();
+  page.textboxEmptySetupCloneUrl.Click(280, 11);
+  page.panel.panel.panel.panel.panel.panel.panel.panel.vg.Click(12, 4);
+}
